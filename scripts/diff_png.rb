807c0cb999a078eb099149d822997bf6fd05df4a
Ian Katz <iakatz@paypal.com>
2014-04-24 14:18:43 -0400

    add the ability to write an image showing the ignored/changed pixels

--- scripts/diff_png.rb
+++ scripts/diff_png.rb
@@ -1,14 +1,15 @@
 #!/usr/bin/env ruby
 require 'rubygems'
 require 'chunky_png'
-
+include ChunkyPNG::Color
 
 if 2 > ARGV.length
   puts
-  puts "Usage: diff_png.rb <file1> <file2>"
+  puts "Usage: diff_png.rb <file1> <file2> [diff]"
   puts
   puts "Compare two images and print the number of pixels that differ."
-  puts "  file1 may contain pixel values of #FF00FF, which will be ignored"
+  puts "  - file1 may contain pixel values of #FF00FF, which will be ignored"
+  puts "  - If diff path is provided, a diff image will be written in PNG format"
   puts
   exit 1
 end
@@ -21,9 +22,11 @@ $stderr.print "Loading candidate image..."
 img2 = ChunkyPNG::Image.from_file(ARGV[1])
 $stderr.puts "done: #{img2.metadata}"
 
+
 diff = 0
 mask = 0
 maskpixel = ChunkyPNG::Color.rgb(255, 0, 255) # same as ChunkyPNG::Color.rgba(255, 0, 255, 255)
+output = ChunkyPNG::Image.new(img1.width, img1.height, maskpixel)
 
 $stderr.print "processing"
 img1.height.times do |y|
@@ -31,8 +34,15 @@ img1.height.times do |y|
   img1.row(y).each_with_index do |pixel, x|
     if pixel == maskpixel
       mask += 1
+      output[x,y] = ChunkyPNG::Color.rgb(0, 0, 0) # black - ignored
     elsif pixel != img2[x,y]
       diff += 1
+      output[x,y] = ChunkyPNG::Color.rgb(255, 0, 0) # red - wrong
+    else
+      # light shade for diff
+      output[x,y] = ChunkyPNG::Color.rgb(r(pixel) / 3 + 169,
+                                         g(pixel) / 3 + 169,
+                                         b(pixel) / 3 + 169)
     end
   end
 end
@@ -44,5 +54,13 @@ puts "pixels masked: #{mask}"
 puts "pixels changed: #{diff}"
 
 if 0 < diff
+
+  # write diff if path is provided
+  if 2 < ARGV.length
+    $stderr.print "Saving diff file to " + ARGV[2] + "..."
+    output.save(ARGV[2])
+    $stderr.puts "done"
+  end
+
   exit 1
 end
