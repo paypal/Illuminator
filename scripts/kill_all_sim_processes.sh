2ab7517fdcbe1bedc76c2f04690439809a62cd1f
Ian Katz <iakatz@paypal.com>
2014-08-04 15:25:45 -0400

    add a description of what's being killed

--- scripts/kill_all_sim_processes.sh
+++ scripts/kill_all_sim_processes.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
 # kill simulator
+echo "Killing all top-level simulator processes..."
 killall -9 "iPhone Simulator"
 
 # kill xpcproxy_sim zombies by explicitly killing parent process
+echo "Killing any xpcproxy_sim zombies..."
 ps -edf | \
  grep [x]pcproxy_sim | awk '{print $3}' | \
  sort | uniq | \
