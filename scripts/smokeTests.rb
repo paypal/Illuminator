3456cbb365334ad310c4a930a152511145d09fcc
Ian Katz <iakatz@paypal.com>
2014-11-04 11:50:02 -0500

    fix device specification in smoke test

--- scripts/smokeTests.rb
+++ scripts/smokeTests.rb
@@ -1,10 +1,15 @@
 require 'pathname'
-require File.join(File.expand_path(File.dirname(__FILE__)), '/classes/IlluminatorFramework.rb')
+
+require File.join(File.expand_path(File.dirname(__FILE__)), 'classes/IlluminatorFramework.rb')
+require File.join(File.expand_path(File.dirname(__FILE__)), 'classes/XcodeUtils.rb')
+
 Dir.chdir 'SampleApp/AutomatorSampleApp'
 workspace = Dir.pwd
+
 #ruby ../../scripts/automationTests.rb -s AutomatorSampleApp -t smoke -p ../../SampleApp/SampleTests/tests/AllTests.js
 allTestPath = '../../SampleApp/SampleTests/tests/AllTests.js'
 allTestPath = (Pathname.new (allTestPath)).realpath.to_s
+
 ####################################################################################################
 # Storing custom parameters
 ####################################################################################################
@@ -16,7 +21,7 @@ options['implementation'] = 'iPhone'
 options['appName'] = 'AutomatorSampleApp'
 options['scheme'] = 'AutomatorSampleApp'
 options['simVersion'] = '7.1'
-options['simDevice'] = 'iPhone Retina (4-inch)'
+options["simDevice"] = 'iPhone 5'
 options['simLanguage'] = 'en'
 options['tagsAny'] = 'smoke'
 options['testPath'] = allTestPath
@@ -24,6 +29,10 @@ options['timeout'] = 30
 options['verbose'] = FALSE
 options['report'] = TRUE
 options['skipBuild'] = FALSE
+
+if XcodeUtils.instance.isXcodeMajorVersion 5
+  options['simDevice'] = 'iPhone Retina (4-inch)'
+end
 
 success = IlluminatorFramework.runWithOptions options, workspace
 exit 1 unless success
