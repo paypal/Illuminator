32fb8f42dd7ae970042e21a9d63e9501a18437b8
Ian Katz <iakatz@paypal.com>
2014-11-05 13:44:11 -0500

    more readable output from script

--- scripts/kill_all_sim_processes.sh
+++ scripts/kill_all_sim_processes.sh
@@ -2,16 +2,16 @@
 
 # kill simulator
 echo "Killing all top-level Xcode 5 simulator processes..."
-killall -9 "iPhone Simulator"
+killall -9 "iPhone Simulator" 2>&1
 echo "Killing all top-level Xcode 6 simulator processes..."
-killall -9 "iOS Simulator"
+killall -9 "iOS Simulator" 2>&1
 
 echo "Killing ScriptAgent..."
-killall -9 "ScriptAgent"
+killall -9 "ScriptAgent" 2>&1
 
 # kill xpcproxy_sim zombies by explicitly killing parent process
 echo "Killing any xpcproxy_sim zombies..."
 ps -edf | \
  grep [x]pcproxy_sim | awk '{print $3}' | \
  sort | uniq | \
- xargs -I{} echo "kill -9 {}" | sh
+ xargs -I{} echo "kill -9 {} 2>&1" | sh
