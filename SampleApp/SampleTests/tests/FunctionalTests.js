1470c3ea6ad7bff72b7ca033b9731d43f77f1287
Ian Katz <iakatz@paypal.com>
2015-03-12 11:29:46 -0400

    better test tag

--- SampleApp/SampleTests/tests/FunctionalTests.js
+++ SampleApp/SampleTests/tests/FunctionalTests.js
@@ -3,7 +3,7 @@
 var ia = appmap.apps["Illuminator"];  // shortcut reference to built-in Illuminator test steps
 var app = appmap.apps["SampleApp"];   // shortcut reference to sample app test steps
 
-automator.createScenario("Wait for an element to appear", ["functional", "smoke"])
+automator.createScenario("Wait for an element to appear", ["functional", "smoke", "wait"])
     .withStep(app.homeScreen.openWait)
     .withStep(app.waitForMe.verifyDelayedMessage, {expected: false})
     .withStep(ia.do.delay, {seconds: 5})
