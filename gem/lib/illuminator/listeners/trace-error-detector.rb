57ed6d602200440fb712bbc6ed24913dc119ab82
Ian Katz <iakatz@paypal.com>
2015-06-03 15:50:17 -0400

    [instruments runner] Better handling of trace errors

---
+++ gem/lib/illuminator/listeners/trace-error-detector.rb
@@ -0,0 +1,34 @@
+
+module TraceErrorDetectorEventSink
+
+  # fatal means that restarting instruments won't fix it
+  def trace_error_detector_triggered(fatal, message)
+    puts "  +++ If you're seeing this, #{self.class.name}.#{__method__} was not overridden"
+  end
+
+end
+
+# TraceErrorDetector monitors the logs for things that indicate a transient failure to start instruments
+#  - "unable to install app"
+#  - etc
+class TraceErrorDetector < InstrumentsListener
+
+  attr_accessor :event_sink
+
+  def trigger(fatal, message)
+    @event_sink.trace_error_detector_triggered(fatal, message)
+  end
+
+  def receive message
+    if message.full_line =~ /Instruments Trace Error : Target failed to run: Unable to install app with path:/
+      trigger(false, "Failed to install app because #{message.full_line.split(': ')[-1]}")
+    elsif message.full_line =~ /Instruments Trace Error/i
+      trigger(false, message.full_line.split(' : ')[1..-1].join)
+    end
+
+  end
+
+  def on_automation_finished
+  end
+
+end
