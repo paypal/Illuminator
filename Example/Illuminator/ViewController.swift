c97c9f4a66ebd9657cf881808a00409446090599
kviksilver <berceg@paypal.com>
2015-10-15 16:09:24 -0400

    added basic cocoapod structure

---
+++ Example/Illuminator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Illuminator
+//
+//  Created by kviksilver on 10/15/2015.
+//  Copyright (c) 2015 kviksilver. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+        
+    
+    }
+
+}
+
