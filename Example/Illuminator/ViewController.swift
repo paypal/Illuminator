ccb42059440c1114b102f3bff3f265641b92fc9e
kviksilver <berceg@paypal.com>
2015-10-16 10:36:36 -0400

    added proper UITest target

--- Example/Illuminator/ViewController.swift
+++ Example/Illuminator/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Illuminator
 
 class ViewController: UIViewController {
 
@@ -19,6 +20,10 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
         
+        
+        _ = XCTUIBridge.register("test") {
+            
+        }
     
     }
 
