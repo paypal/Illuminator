a1a1f1baca24e4372ecc75636624af1c4927a734
Ian Katz <ikatz@wayfair.com>
2016-12-01 11:13:35 -0500

    whitespace

--- Pod/Illuminator/IlluminatorTestCaseWrapper.swift
+++ Pod/Illuminator/IlluminatorTestCaseWrapper.swift
@@ -13,34 +13,33 @@ import XCTest
 public class IlluminatorTestcaseWrapper {
     let testCase: XCTestCase
     var continueAfterFailures = [Bool]()
-    
+
     init(testCase t: XCTestCase) {
         testCase = t
         initContinueAfterFailure()
     }
-    
+
     func initContinueAfterFailure() {
         continueAfterFailures = [testCase.continueAfterFailure]
         sanityCheck()
     }
-    
+
     func pushContinueAfterFailure(val: Bool) {
         sanityCheck()
         continueAfterFailures.append(val)
         testCase.continueAfterFailure = val
     }
-    
+
     func popContinueAfterFailure() {
         sanityCheck()
         continueAfterFailures.removeLast()
         testCase.continueAfterFailure = continueAfterFailures.last!
     }
-    
+
     func sanityCheck() {
         if testCase.continueAfterFailure != continueAfterFailures.last! {
             //TODO: some kind of warning
         }
     }
-    
-}
 
+}
